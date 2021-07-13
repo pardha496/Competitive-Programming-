66475e276833979075143e08107d1018f9dd5686
pardha496 <pardha.saradhi496@msitprogram.net>
2021-07-13 23:57:55 +0530

    first

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -6,4 +6,12 @@
 
 
 def fun_isfactor(f, n):
-	return False # replace with your solution
+    if (f==0&n==0):
+        return True
+    if(f==0):
+      return False
+    if(n==0):
+        return True
+    elif(abs(n)%abs(f)==0):
+        return True         
+    return False # replace with your solution
