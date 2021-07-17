383d33e3225ba5ae5992408e48183f428e6b5087
pardha496 <pardha.saradhi496@msitprogram.net>
2021-07-17 10:16:21 +0530

    s

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -27,6 +27,7 @@ class LinkedList(object):
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
         self.head = new_element
+        
 
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
