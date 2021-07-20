edf6ae98c1bbf63fabf1f6ec32aec87cb370f827
pardha496 <pardha.saradhi496@msitprogram.net>
2021-07-20 15:52:03 +0530

    f

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -5,10 +5,13 @@
 # For example:
 # lookAndSay([]) == []
 # lookAndSay([1,1,1]) == [(3,1)]
-# lookAndSay([-1,2,7]) == [(1,-1),(1,2),(1,7)]
+# lookAndSay([-1,2,'7]) == [(1,-1),(1,2),(1,7)]
 # lookAndSay([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)]
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
 
 def lookandsay(a):
 	# Your code goes here
-	pass
+	b=[]
+	for i in a:
+		count=a.count(i)
+		
