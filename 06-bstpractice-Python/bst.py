c59d87d831849379d669375ac3c30132eed61e91
pardha496 <pardha.saradhi496@msitprogram.net>
2021-07-27 17:49:01 +0530

    f

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -1,22 +1,46 @@
+import bst
 class Node(object):
-    def __init__(self, value):
+    def _init_(self, value):
         self.value = value
         self.left = None
         self.right = None
 
-class BST(object):
-    def __init__(self, root):
+class BinaryTree(object):
+    def _init_(self, root):
         self.root = Node(root)
 
-    def insert(self, new_val):
-        # Your code goes here
-        pass
-
-    def printSelf(self):
-        # Your code goes here
-        pass
-        
     def search(self, find_val):
+        """
+        Return True if the find_val is in the tree and False otherwise.
+        """
         # Your code goes here
-        pass
+        return self.preorder_search(self.root,find_val)
 
+    def print_tree(self):
+        """
+        Print out all tree nodes as they are visited in a pre-order traversal."""
+        # Your code goes here
+        self.preorder_print(self.root)
+
+    def preorder_search(self, start, find_val):
+        """
+        Helper method - use this to create a recursive search solution.
+        """
+        # Your code goes here
+        if start ==None:
+            return False
+        if start.value == find_val:
+            return True
+        return self.preorder_search(start.left,find_val) or self.preorder_search(start.right,find_val)
+
+
+    def preorder_print(self, start, traversal):
+        """
+        Helper method - use this to create a recursive print solution.
+        """
+        # Your code goes here
+        if start is None:
+            return 
+        print(start.value)
+        self.preorder-print(start.left)
+        self.preorder_print(start.right)
