0b52ef3cea7bae0db8ac0c71676f9399bb22d332
pardha496 <pardha.saradhi496@msitprogram.net>
2021-07-27 17:52:15 +0530

    f

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -3,7 +3,6 @@ class Node(object):
         self.value = value
         self.left = None
         self.right = None
-
 class BinaryTree(object):
     def _init_(self, root):
         self.root = Node(root)
@@ -13,33 +12,28 @@ class BinaryTree(object):
         Return True if the find_val is in the tree and False otherwise.
         """
         # Your code goes here
-        return self.preorder_search(self.root,find_val)
+        return self.preorder_search(self.root, find_val)
 
     def print_tree(self):
         """
         Print out all tree nodes as they are visited in a pre-order traversal."""
         # Your code goes here
-        self.preorder_print(self.root)
+        return self.preorder_print(self.root, "")[:-1]
 
     def preorder_search(self, start, find_val):
         """
         Helper method - use this to create a recursive search solution.
         """
         # Your code goes here
-        if start ==None:
-            return False
-        if start.value == find_val:
-            return True
-        return self.preorder_search(start.left,find_val) or self.preorder_search(start.right,find_val)
-
+        return start.value == find_val or self.preorder_search(start.left, find_val) or self.preorder_search(start.right, find_val) if start else False
 
     def preorder_print(self, start, traversal):
         """
         Helper method - use this to create a recursive print solution.
         """
         # Your code goes here
-        if start is None:
-            return 
-        print(start.value)
-        self.preorder-print(start.left)
-        self.preorder_print(start.right)
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
