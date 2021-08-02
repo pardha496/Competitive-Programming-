70ee1a0c7a6e15f4f0e2c37ad9c230d8068d42af
pardha496 <pardha.saradhi496@msitprogram.net>
2021-08-02 16:16:38 +0530

    f

--- 01-isrectangular-Python/isrectangular.py
+++ 01-isrectangular-Python/isrectangular.py
@@ -4,8 +4,11 @@
 
 
 def fun_isrectangular(l):
-	# Your code goes here...
-	return False
+    # Your code goes here...
+    for i  in l:
+        if len(i) != len(l[0]):
+            return False
+    return True
 
 
 
